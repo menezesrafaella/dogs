52c96aa3cca8ecbf8b52f2d8c2e1d287c4157e12
Rafaella <carmo.rafaella@yahoo.com.br>
2022-03-13 10:12:17 -0300

    style: page login

---
+++ src/Components/Helper/Error.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Error = ({error}) => {
+    if(!error) return null;
+  return (
+    <span style={{color: '#f31', margin: '1rem 0'}}>{error}</span>
+  )
+}
+
+export default Error
