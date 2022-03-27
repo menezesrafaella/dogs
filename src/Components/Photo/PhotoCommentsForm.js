2a76a8804a4273f9cfdebb6178c28d4eedf6f4d1
Rafaella <carmo.rafaella@yahoo.com.br>
2022-03-27 12:08:56 -0300

    feat: post comment and style

---
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { ReactComponent as Send } from "../../Assets/send.svg";
+import { COMMENT_POST } from "../../env";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import styles from './PhotoCommentsForm.module.css'
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+    const {request, error} = useFetch();
+  const [comment, setComment] = React.useState("");
+
+  async function handleSubmit(event) {
+      event.preventDefault();
+      const {url, options} = COMMENT_POST(id, {comment})
+      const {response, json} = await request(url, options)
+      if(response.ok) {
+          setComment('')
+          setComments((comments) => [...comments,json])
+      }
+  }
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <textarea
+        className={styles.textarea}
+        value={comment}
+        id="comment"
+        name="comment"
+        placeholder="Comente..."
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button className={styles.button}>
+        <Send />
+      </button>
+      <Error error={error} />
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
