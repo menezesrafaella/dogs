5a4b9fe8671b77ba66f7f44b2865649277d7e290
Rafaella <carmo.rafaella@yahoo.com.br>
2022-03-30 10:33:59 -0300

    feat: infinite scroll

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,17 +6,21 @@ import Loading from '../Helper/Loading'
 import FeedPhotosItem from './FeedPhotosItem'
 import styles from './FeedPhotos.module.css'
 
-const FeedPhotos = ({ page, user, setModalPhoto}) => {
+const FeedPhotos = ({ setInfinite, page, user, setModalPhoto}) => {
     const { data,loading,error,request } = useFetch()
 
     useEffect(() => {
         async function fetchPhotos() {
-            const {url, options} = PHOTOS_GET({page: 1, total: 6, user})
-            const {json} = await request(url,options)
+            const total = 3
+            const {url, options} = PHOTOS_GET({page: 1, total, user})
+            const {response, json} = await request(url,options)
+            if (response && response.ok && json.length < total) {
+                setInfinite(false)
+            }
         }
         fetchPhotos();
         console.log(data)
-    }, [request, user, page])
+    }, [request, user, page, setInfinite])
 
     if (error) return <Error error={error} />
     if (loading) return <Loading />
